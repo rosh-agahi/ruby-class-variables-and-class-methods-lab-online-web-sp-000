a2601ec8b22c7aff6dcd885c21f87a3596a6be31
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-17 23:34:26 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+class Song 
+  attr_accessor :name, :artist, :genre 
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  
+  def initialize(name,artist,genre)
+    @name = name
+    @genre = genre
+    @artist = artist
+    
+    @@count += 1
+    
+    @@genres << genre
+    @@artists << artist
+  end
+  
+  def count
+    @@count
+  end
+  
+end
